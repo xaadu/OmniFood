f5c1caf69fd411c72e36626038b4d6638bbfdb42
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-04-04 03:48:47 +0600

    Completed Sign-Up Section

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -9,5 +9,12 @@ themes.forEach(function(theme) {
         root.style.setProperty('--color-primary',  t.getPropertyValue('--theme-'+theme.id));
         root.style.setProperty('--color-primary-light',  t.getPropertyValue('--theme-'+theme.id+'-light'));
         root.style.setProperty('--color-primary-dark',  t.getPropertyValue('--theme-'+theme.id+'-dark'));
+        root.style.setProperty('--color-primary-rgb',  t.getPropertyValue('--theme-'+theme.id+'-rgb'));
+        root.style.setProperty('--color-primary-light-rgb',  t.getPropertyValue('--theme-'+theme.id+'-light-rgb'));
+        root.style.setProperty('--color-primary-dark-rgb',  t.getPropertyValue('--theme-'+theme.id+'-dark-rgb'));
+        var x = document.getElementsByClassName('theme')[0].getElementsByClassName('active');
+        if(x.length>0 && x[0].classList.contains('active'))
+            x[0].classList.remove('active');
+        theme.classList.add('active');
     });
 })
