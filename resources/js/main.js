f659a828d0d8d7d666e599c10e8f35a2ccdeca85
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-04-06 20:50:55 +0600

    Finished Animations

---
+++ resources/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    /* Sticky Navigation */ 
+    $('.js--section-features').waypoint(function (direction) { 
+        if (direction == 'down') $('.nav').addClass('sticky');
+        else $('.nav').removeClass('sticky');
+     }, {
+         offset: '15%'
+     });
+
+     /* Scoll On Buttons */
+     $('.js--scroll-to-plans').click(function (e) {
+         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+     });
+     $('.js--scroll-to-start').click(function (e) {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 600);
+    });
+
+    /* Animations */
+    $('.js--wp-1').waypoint(function (direction) { 
+        $('.js--wp-1').addClass('animated fadeIn');
+     }, { offset: '60%' });
+
+    $('.js--wp-2').waypoint(function (direction) { 
+        $('.js--wp-2').addClass('animated fadeInUp');
+     }, { offset: '60%' });
+    
+    $('.js--wp-3').waypoint(function (direction) { 
+        $('.js--wp-3').addClass('animated fadeIn');
+     }, { offset: '60%' });
+    
+     $('.js--wp-4').waypoint(function (direction) { 
+        $('.js--wp-4').addClass('animated pulse');
+     }, { offset: '60%' });
+    
+
+});
